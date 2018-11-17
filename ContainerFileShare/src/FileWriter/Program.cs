1b4aa2cce573ce65473ff56a6ceb6e887e437e85
fbeltrao <fbeltrao@windowslive.com>
2018-11-17 12:06:49 +0100

    Add arm32v7 images

---
+++ ContainerFileShare/src/FileWriter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileWriter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var filePath = "./data";
+                if (args != null && args.Length > 0) 
+                {
+                    filePath = args[0];
+                }
+
+                var fileName = Path.Combine(filePath, "shared.txt");
+
+                
+                using (var outStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                {
+                    using (var writer = new StreamWriter(outStream))
+                    {
+                        var count = 0L;
+                        while (true)
+                        {
+                            var text = $"[{DateTime.UtcNow.ToShortTimeString()}] {count}";
+                            Console.WriteLine("Wrote:" + text);
+                            await writer.WriteLineAsync(text);
+                            await writer.FlushAsync();
+                            count++;
+
+                            await Task.Delay(10);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Environment.ExitCode = 1;
+            }
+        }
+    }
+}
