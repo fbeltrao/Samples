ed480d0719391aaffde415dd80e85987ecda15e9
fbeltrao <fbeltrao@windowslive.com>
2018-11-18 11:22:59 +0100

    Add NugetPackageLocalReference

---
+++ NugetPackageLocalReference/src/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v1 = 3123;
+            var v2 = 1.45;
+            Console.WriteLine($"{v1} + {v2} = {new Calculator().Sum(v1, v2)}");
+        }
+    }
+}
---
+++ NugetPackageLocalReference/src/MyLibrary/Calculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Calculator
+    {
+        public double Sum(double v1, double v2) => v1 + v2;
+    }
+}
